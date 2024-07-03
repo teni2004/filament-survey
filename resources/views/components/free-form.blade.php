b20b87c3cdcb813bd7034086f328c6175713350a
teni2004 <oasade@givelify.com>
2024-07-03 12:35:01 +0100

    Free form prepopulates when editing

--- resources/views/components/free-form.blade.php
+++ resources/views/components/free-form.blade.php
@@ -1,2 +1,15 @@
+@php
+if(isset($answers))
+{
+    foreach($answers as $answer)
+    {
+        if($answer->question->id === $question->id)
+        {
+            $text = $answer->free_form_answer->body;
+        }
+    }
+}
+@endphp
+
 <label for="{{$label}}"></label>
-<textarea id="{{$label}}" name="{{$label}}" rows="3" class="px-1.5 bg-gray-100 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-orange sm:text-sm sm:leading-6" {{ $question->required ? 'required' : '' }}></textarea>
+<textarea id="{{$label}}" name="{{$label}}" rows="3" class="px-1.5 bg-gray-100 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-orange sm:text-sm sm:leading-6" {{ $question->required ? 'required' : '' }}>{{ $text ?? '' }}</textarea>
