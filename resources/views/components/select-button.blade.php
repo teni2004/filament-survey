574983f5213f023de19901e36c00d8bad81711dc
teni2004 <oasade@givelify.com>
2024-07-03 12:20:50 +0100

    Select-one prepopulates when editing

--- resources/views/components/select-button.blade.php
+++ resources/views/components/select-button.blade.php
@@ -1,7 +1,7 @@
 
-@props(['op_id', 'name', 'required'])
+@props(['op_id', 'name', 'required', 'selected'])
 
 <div class="flex items-center gap-x-3">
-    <input id="{{ $op_id }}" value="{{ $op_id }}" name="{{ $name }}" type="radio" class="h-4 w-4 border-gray-300 text-orange focus:ring-orange" {{ $required ? 'required' : '' }}>
+    <input id="{{ $op_id }}" value="{{ $op_id }}" name="{{ $name }}" type="radio" class="h-4 w-4 border-gray-300 text-orange focus:ring-orange" {{ $required ? 'required' : '' }} {{ $selected === $op_id  ? 'checked' : '' }}>
     <label for="{{ $op_id }}" class="block text-md font-medium leading-6 text-gray-900">{{ $slot }}</label>
 </div>
--- resources/views/components/select-one.blade.php
+++ resources/views/components/select-one.blade.php
@@ -1,9 +1,21 @@
+@props(['question', 'answers', 'options', 'label'])
+
 @php
-$required = $question->required;
+    $required = $question->required;
+    if(isset($answers))
+    {
+        foreach($answers as $answer)
+        {
+            if($answer->question->id === $question->id)
+            {
+                $selected = $answer->select_one_answer->option_id;
+            }
+        }
+    }
 @endphp
 
 <div class="mt-6 space-y-3">
     @foreach($options as $option)
-        <x-select-button required="{{$required}}" name="{{$label}}" op_id='{{$option->id}}'>{{$option->text}}</x-select-button>
+        <x-select-button required="{{$required}}" name="{{$label}}" op_id='{{$option->id}}' selected="{{$selected ?? ''}}">{{$option->text}}</x-select-button>
     @endforeach
 </div>
